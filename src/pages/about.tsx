3325dcfc9aea5045da11bbdbb1a23aaf8ea86c74
gerarduffy <cgduffman@gmail.com>
2023-09-13 20:33:18 -0700

    Refactor about.tsx to use relative image path for founder's image.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,17 +12,17 @@ export default function About() {
           <div className='m-4'></div>
           <h2 className="font-bold mb-2 border-b-2 border-gray-200 pb-2">Core Features:</h2>
           <ul className="mx-auto mt-4 max-w-xl text-center text-xl leading-8 text-slate-400">
-            <li className=''><span className="font-bold">Personalized AI Companion:</span> Each child will have their own unique virtual companion created just for them. Parents customize their child&apos;s companion based on age, interests, reading level, and more during the setup process.</li>
-            <li className="mb-10"><span className="font-bold my-5">Adaptive Learning:</span> The AI companions leverage advanced natural language processing to have natural conversations and engage with children at their level. As the child grows, the companion adapts and evolves as well.</li>
-            <li className='mb-10'><span className="font-bold my-5">Educational Content:</span> Companions provide interactive learning by reading age-appropriate stories, recommending books, teaching concepts, and even casting educational videos on the TV through device connectivity.</li>
-            <li className='mb-10'><span className="font-bold my-5">Memory Archive:</span> Every conversation and interaction is securely archived to create a digital memory bank that allows the AI to reference its experiences with each child and enhance future discussions.</li>
-            <li className='mb-10'><span className="font-bold my-5">Parental Oversight:</span> Weekly conversation summaries and full transcripts offer complete transparency into the child&apos;s interactions. Parents can configure allowed topics and terms of use.</li>
-            <li className='mb-10'><span className="font-bold my-5">Kid-Friendly Interface:</span> The app features a fun, game-like interface to keep kids engaged. They can customize their companion&apos;s name, look, and personality.</li>
+            <li className='mb-6'><span className="font-bold my-3">Personalized AI Companion:</span> Each child will have their own unique virtual companion created just for them. Parents customize their child&apos;s companion based on age, interests, reading level, and more during the setup process.</li>
+            <li className="mb-6"><span className="font-bold my-3">Adaptive Learning:</span> The AI companions leverage advanced natural language processing to have natural conversations and engage with children at their level. As the child grows, the companion adapts and evolves as well.</li>
+            <li className='mb-6'><span className="font-bold my-3">Educational Content:</span> Companions provide interactive learning by reading age-appropriate stories, recommending books, teaching concepts, and even casting educational videos on the TV through device connectivity.</li>
+            <li className='mb-6'><span className="font-bold my-3">Memory Archive:</span> Every conversation and interaction is securely archived to create a digital memory bank that allows the AI to reference its experiences with each child and enhance future discussions.</li>
+            <li className='mb-6'><span className="font-bold my-3">Parental Oversight:</span> Weekly conversation summaries and full transcripts offer complete transparency into the child&apos;s interactions. Parents can configure allowed topics and terms of use.</li>
+            <li className='mb-6'><span className="font-bold my-3">Kid-Friendly Interface:</span> The app features a fun, game-like interface to keep kids engaged. They can customize their companion&apos;s name, look, and personality.</li>
           </ul>
 
           <div className='m-4'></div><div className='m-4'></div>
           <h2 className="font-bold mb-2 border-b-2 border-gray-200 pb-2">Founder</h2>
-          <Image className="rounded-full" src="/assets/images/headshot.png" alt="Connor Duffy" width={500} height={500} />
+          <Image className="rounded-full" src="/headshot.png" alt="Connor Duffy" width={500} height={500} />
           <h3 className="font-medium mb-2">Connor Duffy</h3>
 
       </div>
