facbac6696f41e86c3e502b874d00b7a62f85790
Connor Gerard Duffy <cgduffman@gmail.com>
2023-11-28 15:53:43 -0800

    fix

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,21 +1,8 @@
-import { SignIn, useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  const { user } = useUser();
-  const router = useRouter();
-
-  // Redirect the user to the main application when they sign in
-  useEffect(() => {
-    if (user) {
-      router.push("/");
-    }
-  }, [user, router]);
-
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <SignIn />
     </div>
   );
 }
